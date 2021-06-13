f69ff23da49ffc75880dc95c356392e9f1e0a584
Julien Calixte <juliencalixte@gmail.com>
2021-06-13 10:34:01 +0200

    :sparkles: (allure)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <h1 class="title is-1">Allure min/km</h1>
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png" />
-    <HelloWorld msg="Welcome to OVA Template!" />
+  <div class="home columns is-centered">
+    <div class="field column is-one-third">
+      <div class="control">
+        <input
+          v-model.number="minute"
+          class="input"
+          type="number"
+          placeholder="minute"
+        />
+      </div>
+    </div>
+    <div class="field column is-one-third">
+      <div class="control">
+        <input
+          v-model.number="second"
+          class="input"
+          type="number"
+          placeholder="second"
+        />
+      </div>
+    </div>
   </div>
+  {{ minute }}
+  {{ second }}
+  <main>{{ allure }} km/h</main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed, defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const minute = ref<number | null>(null)
+    const second = ref<number | null>(null)
+
+    const allure = computed(() => {
+      if (minute.value === null && second.value === null) {
+        return '0'
+      }
+
+      const time = (minute.value ?? 0) * 60 + (second.value ?? 0)
+
+      const velocity = (1000 / time) * 3.6
+
+      return velocity.toLocaleString('fr', {
+        maximumFractionDigits: 2
+      })
+    })
+
+    return {
+      minute,
+      second,
+      allure
+    }
   }
 })
 </script>
+
+<style scoped>
+main {
+  font-size: 5rem;
+}
+</style>
